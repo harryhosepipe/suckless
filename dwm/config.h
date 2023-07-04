f7b0e49690dd0ab2277603d607d9b298f6a6beaa
Pablo - Flux <pablo@renderbros.com>
2023-07-04 16:16:39 +0200

    Update files

--- dwm/config.h
+++ dwm/config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Terminess Nerd Font:size=14" };
+static const char *fonts[]          = { "Terminess Nerd Font:size=12" };
 static const char dmenufont[]       = "Terminess Nerd Font:size=22";
 static const char col_gray1[]       = "#333333";
 static const char col_gray2[]       = "#444444";
